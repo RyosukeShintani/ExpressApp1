67eabc7c2afd3a98f8d1728778403a2d32157f35
mozuku55 <ryo.tako.05@gmail.com>
2023-10-09 09:26:53 +0900

    env修正

--- ExpressApp1/CosmosClient.js
+++ ExpressApp1/CosmosClient.js
@@ -1,12 +1,9 @@
 const { CosmosClient } = require('@azure/cosmos');
 const dotenv = require('dotenv')
-dotenv.config({ path: './.env' });
-
-// const endpoint = process.env.COSMOS_ENDPOINT; // ���ϐ�����G���h�|�C���g���擾
-// const key = process.env.COSMOS_KEY; // ���ϐ�����L�[���擾
-
-const endpoint = 'https://bigbrother.documents.azure.com:443/'
-const key = 'TEa0iSj24m68YiPLnCVYSMKxwMRPpBRFfONcr24zt00wc7Z0VeW6smbXFInwO20VD7USNnSUzF2IACDbJaek2w'
+// dotenv.config({ path: './.env' });
+dotenv.config();
+const endpoint = process.env.COSMOS_ENDPOINT; // ���ϐ�����G���h�|�C���g���擾
+const key = process.env.COSMOS_KEY; // ���ϐ�����L�[���擾
 
 var client = new CosmosClient({ endpoint: endpoint, key: key });
 
